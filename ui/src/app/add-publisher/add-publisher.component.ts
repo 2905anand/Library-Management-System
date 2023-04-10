fd709c41e5386321f16deca1a0eb99b1117e3b24
Anand Sharma <2002anandsharma@gmail.com>
2023-04-10 20:54:21 +0530

    Add-Publisher Function Added

--- ui/src/app/add-publisher/add-publisher.component.ts
+++ ui/src/app/add-publisher/add-publisher.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder , FormGroup, Validators} from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-publisher',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-publisher.component.css']
 })
 export class AddPublisherComponent implements OnInit {
-
-  constructor() { }
+  publishersForm : FormGroup;
+  constructor(private formbuilder: FormBuilder, private http: HttpClient, private router: Router ) { 
+    this.publishersForm = this.formbuilder.group({
+      ID: [],
+      name: ['', Validators.required],
+      address: [],
+      phone: [],
+      email: [],
+      
+    })
+  }
 
   ngOnInit(): void {
   }
+
+  savePublisher(){
+    // Make Post call to request url http://localhost:8080/publisher/savePublisher
+    
+    let publisherData = this.publishersForm.value;
+    // Handle date to string
+    this.http.post('http://localhost:8080/publisher/savePublisher',publisherData)
+    .subscribe(response => {
+      console.log('Publisher saved to DB', response)
+      this.router.navigateByUrl('/publishers')
+    }, error =>{
+      console.error("Error in publisher save", error)
+    }
+    );
+
+  }
+
+  
+
+
 
 }
--- ui/src/app/publishers/publishers.component.ts
+++ ui/src/app/publishers/publishers.component.ts
@@ -1,15 +1,54 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-publishers',
+  selector: 'app-publisher',
   templateUrl: './publishers.component.html',
   styleUrls: ['./publishers.component.css']
 })
 export class PublishersComponent implements OnInit {
 
-  constructor() { }
+  title = 'Publisher Management'
+  publishers: any = [];
+  //books = []
+
+  isGreen = true
+  
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAllPublisher()
+  }
+
+  addPublishers(){
+   
+    console.log("addPublishers button clicked!!")
+    // Take user to /add-books url
+    this.router.navigateByUrl('/add-publisher')
+  }
+
+  getAllPublisher(){
+    this.http.get("http://localhost:8080/publisher/getAllPublisher")
+    .subscribe(resp =>{
+      this.publishers = resp;
+      console.log('Students retrieved successfully:', this.publishers)
+    }, error => {
+      console.error('Error retrieving books:', error);
+    });
+  }
+
+  deletePublisher(publisherId:Number){
+    
+    const url = 'http://localhost:8080/publisher/delete/' +publisherId
+    console.log(url)
+    this.http.delete(url)
+    .subscribe(resp => {
+      console.log('Student deleted successfully');
+      this.getAllPublisher()
+    }, error => {
+      console.error('Error deleting book:', error);
+    });
   }
 
 }
